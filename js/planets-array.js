53674ec7235943a97c30e16fe0ff3c180582180a
Justin Reich <justinreich.dev@gmail.com>
2020-10-06 13:54:38 -0500

    feat: Add solution to Array Manipulation exercises

---
+++ js/planets-array.js
@@ -0,0 +1,48 @@
+(function(){
+    "use strict";
+
+    var planets = [
+        'Mercury',
+        'Venus',
+        'Earth',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+    ];
+
+    /**
+     * TODO:
+     * Read each console log below, and write some javascript code to perform
+     * the step that it describes
+     */
+
+    console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift("The Sun");
+    console.log(planets);
+
+    console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push("Pluto");
+    console.log(planets);
+
+    console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift();
+    console.log(planets);
+
+    console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop();
+    console.log(planets);
+
+    console.log('Finding and logging the index of "Earth" in the planets array.');
+    var indexOfEarth = planets.indexOf("Earth");
+    console.log('Index of Earth: ' + indexOfEarth);
+
+    console.log("Reversing the order of the planets array.");
+    planets.reverse();
+    console.log(planets);
+
+    console.log("Sorting the planets array.");
+    planets.sort();
+    console.log(planets);
+})();
