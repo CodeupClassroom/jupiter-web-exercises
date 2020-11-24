aca87f75d5f6cbeca0af0ed12d505138e683233b
Justin Reich <justinreich.dev@gmail.com>
2020-11-24 09:43:06 -0600

    feat: Add solution to promises GitHub API exercise

--- js/promises.js
+++ js/promises.js
@@ -11,11 +11,36 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
  */
 
-function wait(numMilliseconds) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, numMilliseconds)
-    });
+// function wait(numMilliseconds) {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, numMilliseconds)
+//     });
+// }
+//
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+
+// function to return last push date
+
+function extractDateOfLastPushEvent(events) {
+    for (let event of events) {
+        if (event.type === 'PushEvent') {
+            return new Date(event.created_at).toDateString();
+        }
+    }
+    return null;
 }
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+function lastPushDate(username) {
+    const url = `https://api.github.com/users/${username}/events`;
+    const options = {headers: {'Authorization': 'token ' + gitHubToken}};
+    return fetch(url, options)
+        .then(res => res.json())
+        .then(extractDateOfLastPushEvent)
+        .catch(console.error);
+}
+
+lastPushDate("jreich5").then(console.log);
+
